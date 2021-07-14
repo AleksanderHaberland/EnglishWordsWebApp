7df752a3ce496cab9955342a3a3a18956f35cbe7
aleks <aleksanderhabe@gmail.com>
2021-07-14 12:21:45 +0200

    Intial Commit

---
+++ src/main/java/pl/project/englishwordswebapp/EnglishWordsWebAppApplication.java
@@ -0,0 +1,13 @@
+package pl.project.englishwordswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishWordsWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishWordsWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/project/englishwordswebapp/EnglishWordsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.englishwordswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishWordsWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
